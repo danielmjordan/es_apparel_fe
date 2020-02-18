414db314300a09292ae793910508cefec3496ce5
Daniel Jordan <danieljordan86@gmail.com>
2020-02-18 12:04:09 -0700

    Stub server routes in express

--- server/index.js
+++ server/index.js
@@ -8,9 +8,26 @@ const PORT = 3000;
 app.use(bodyParser.json());
 app.use(cors());
 
-
+//parameters --> are count, sort, and page
 app.get('/reviews/:productId/list', (req, res) => {
   res.json({ info: 'Information shipped here' });
+  res.status(200).send('main get route served');
+});
+
+app.get('/reviews/:productId/meta', (req, res) => {
+  res.status(200).send('Metadata returned from this end point');
+});
+
+app.post('/reviews/:productId', (req, res) => {
+  res.status(201).send('post route served');
+});
+
+app.put('/reviews/helpful/:review_id', (req, res) => {
+  res.status(204).send('Helpful PUT route served');
+});
+
+app.put('/reviews/report/:review_id', (req, res) => {
+  res.status(204).send('Review reported PUT served');
 });
 
 
