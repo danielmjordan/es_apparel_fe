67a683ec51b69a0db9324a5ec5df08247c74b980
Daniel Jordan <danieljordan86@gmail.com>
2020-02-26 10:36:09 -0700

    Isolate reviews microservice

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,6 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import './App.scss';
-import Overview from './components/overview-components/overview/overview.component';
-import RelatedProducts from './components/related-products-components/related-products/related-products.component';
-import QnA from './components/qna-components/qna/qna.component';
 import Reviews from './components/review-components/reviews/reviews.component';
 
 class App extends React.Component {
@@ -20,9 +17,6 @@ class App extends React.Component {
     const { productId } = this.state;
     return (
       <div className="App">
-        {/* <Overview id={productId} />
-        <RelatedProducts id={productId} />
-        <QnA id={productId} /> */}
         <Reviews id={productId} />
       </div>
     );
